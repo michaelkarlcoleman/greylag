42dcccd27dae2ce63b538379a060080bd274ebfd
Mike Coleman <mkc@mkc-laptop.(none)>
2008-04-22 01:50:01 -0500

    validate: FDR adjustment, fix marking of bad charges
    FDR adjustment will search for an internal FDR that will give an effective FDR
    as close as possible to what the user specified.  This gives a few extra real
    ids.
    
    Fixed a bug that was causing "horrible" charges (e.g., +2 having only decoy
    results) not to be marked/killed.

--- greylag_validate.py
+++ greylag_validate.py
@@ -159,10 +159,9 @@ def set_marks(options, thresholds, sp_scores, spectra, kill=False):
         mark_spectrum = True
         if sp_scores[spectrum_no]:
             charge, score, delta = sp_scores[spectrum_no]
-            mark_spectrum = (charge in thresholds
-                             and score != None
-                             and (score < thresholds[charge][0]
-                                  or delta < thresholds[charge][1]))
+            mark_spectrum = (charge not in thresholds
+                             or (score < thresholds[charge][0]
+                                 or delta < thresholds[charge][1]))
         if mark_spectrum and not kill:
             ML_lines = [ (set_mark(M_line), mod_lines, L_lines)
                          for M_line, mod_lines, L_lines in ML_lines ]
@@ -313,10 +312,10 @@ def calculate_inner_threshold(fdr, charge, spinfo):
     return (current_threshold, real_count, decoy_count)
 
 
-def calculate_combined_thresholds(options, z_scores):
+def calculate_combined_thresholds(fdr, options, z_scores):
     """Find best score/delta thresholds for each charge."""
 
-    ppv_goal = 1 - options.fdr
+    ppv_goal = 1 - fdr
 
     # Rather than search every possible value of delta, we're only going to
     # "sample" at this granularity.  This cuts search time dramatically (and
@@ -326,8 +325,6 @@ def calculate_combined_thresholds(options, z_scores):
 
     # charge -> (score, delta, passing_reals, passing_decoys)
     thresholds = {}
-
-    total_reals, total_decoys = 0, 0
 
     for charge, spinfo in z_scores.iteritems():
         spinfo0 = sorted(spinfo, key=lambda x: x[1], reverse=True)
@@ -340,7 +337,7 @@ def calculate_combined_thresholds(options, z_scores):
                 or abs(this_value - last_value) >= SEARCH_GRANULARITY):
 
                 # "inner" is score
-                r = calculate_inner_threshold(options.fdr, charge, spinfo0)
+                r = calculate_inner_threshold(fdr, charge, spinfo0)
                 if r[0] != None:
                     if options.debug:
                         print '#', charge, r[0], this_value, r[1], r[2]
@@ -351,30 +348,73 @@ def calculate_combined_thresholds(options, z_scores):
                 last_value = this_value
             spinfo0.pop()
 
-        if charge in thresholds:
-            score_threshold, delta_threshold = thresholds[charge][0:2]
+    return thresholds
 
-            if options.list_peptides:
-                for (score, delta, state,
-                     spectrum_name, peptide) in z_scores[charge]:
-                    if score >= score_threshold and delta >= delta_threshold:
-                        print "%+d %s %s %s" % (charge, spectrum_name, state,
-                                                peptide)
-            else:
-                reals, decoys = thresholds[charge][2], thresholds[charge][3]
-                total_reals += reals
-                total_decoys += decoys
-                print ("%+d: score %f, delta %.6f -> %s real ids, %s decoys"
-                       " (fdr %.4f)"
-                       % (charge, score_threshold, delta_threshold,
-                          reals, decoys, 0.5 * (1 - PPV(reals, decoys))))
+
+def search_adjusting_fdr(options, z_scores):
+    """Repeatedly calculate thresholds while adjusting FDR, looking for an FDR
+    guess that results in an appropriate goal FDR.  This is needed because the
+    thresholds found for per-charge FDRs will be saturated or have
+    quantization errors, resulting in overly conservative thresholds.
+    """
+
+    def try_fdr(fdr_guess):
+        thresholds = calculate_combined_thresholds(fdr_guess, options,
+                                                   z_scores)
+        charges = sorted(thresholds.keys())
+        total_reals = sum(thresholds[charge][2] for charge in charges)
+        total_decoys = sum(thresholds[charge][3] for charge in charges)
+        fdr_result = 1 - PPV(total_reals, total_decoys)
+        return fdr_result, thresholds, charges, total_reals, total_decoys
+
+    low_fdr, high_fdr = options.fdr, min(2*options.fdr, 0.4999999999999)
+    low_results = try_fdr(low_fdr)
+    high_results = try_fdr(high_fdr)
+    initial_total_reals = low_results[3]
+
+    #    #
+    #    l        g            h
+    #   l       g         h
+    #    #
+
+    for i in range(32):
+        if abs(high_fdr - low_fdr) <= 0.01*options.fdr:
+            break
+        guess_fdr = (high_fdr + low_fdr) / 2.0
+        guess_results = try_fdr(guess_fdr)
+        if options.debug:
+            print ("%s %s %s -> %s"
+                   % (low_fdr, guess_fdr, high_fdr, guess_results[0]))
+        if guess_results[0] > options.fdr:
+            high_fdr, high_results = guess_fdr, guess_results
         else:
-            warn("could not calculate thresholds for %+d" % charge)
+            low_fdr, low_results = guess_fdr, guess_results
+    else:
+        warn("FDR adjustment convergence failed")
+
+    fdr_result, thresholds, charges, total_reals, total_decoys = low_results
+
+    for charge in charges:
+        score_threshold, delta_threshold = thresholds[charge][0:2]
+
+        if options.list_peptides:
+            for (score, delta, state,
+                 spectrum_name, peptide) in z_scores[charge]:
+                if score >= score_threshold and delta >= delta_threshold:
+                    print "%+d %s %s %s" % (charge, spectrum_name, state,
+                                            peptide)
+        else:
+            reals, decoys = thresholds[charge][2], thresholds[charge][3]
+            print ("%+d: score %f, delta %.6f -> %s real ids, %s decoys"
+                   " (fdr %.4f)"
+                   % (charge, score_threshold, delta_threshold,
+                      reals, decoys, 0.5 * (1 - PPV(reals, decoys))))
 
     if not options.list_peptides:
         print ("# total: %s real ids, %s decoys (fdr %.4f)"
-               % (total_reals, total_decoys,
-                  0.5 * (1 - PPV(total_reals, total_decoys))))
+               % (total_reals, total_decoys, fdr_result / 2.0))
+        print ("# (FDR adjustment found %s more real ids)"
+               % (total_reals - initial_total_reals))
 
     return thresholds
 
@@ -458,7 +498,7 @@ def main(args=sys.argv[1:]):
                % (sum(1 for x in spectrum_scores if x),
                   sum(len(z_scores[x]) for x in z_scores)))
 
-    thresholds = calculate_combined_thresholds(options, z_scores)
+    thresholds = search_adjusting_fdr(options, z_scores)
 
     if options.debug:
         pprint(thresholds)
